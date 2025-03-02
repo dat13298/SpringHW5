58044a3a65b91394d0e2a2a5fabbc210edcc1abc
dat13298 <157354116+dat13298@users.noreply.github.com>
2025-03-02 15:16:17 +0700

    enable Auditing

--- src/main/java/com/aptech/springfhw5/SpringFhw5Application.java
+++ src/main/java/com/aptech/springfhw5/SpringFhw5Application.java
@@ -2,8 +2,10 @@ package com.aptech.springfhw5;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class SpringFhw5Application {
 
     public static void main(String[] args) {
